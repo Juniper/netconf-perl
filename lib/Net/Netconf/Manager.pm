17672ead52dca7611aeb62e313c14040650775e4
Priyal Jain <jainpriyal168@gmail.com>
2014-12-22 11:33:15 +0530

    latest version 1.00

--- lib/Net/Netconf/Manager.pm
+++ lib/Net/Netconf/Manager.pm
@@ -1,7 +1,7 @@
 package Net::Netconf::Manager;
 
 use Carp;
-our $VERSION ='0.01';
+our $VERSION ='1.00';
 
 # This instantiates a Junoscript or a Netconf device depending on the 'server'
 # specified. Default is Netconf.
